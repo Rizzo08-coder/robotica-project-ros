b5cd89716856bca7d1476f86b7b8bea3c21456a3
Matteo Rizzo <m.rizzo009@studenti.unibs.it>
2024-04-30 16:26:39 +0200

    /move-joints with joints parameters

--- cobotta_rest_api/cobotta_rest_api/flask_api.py
+++ cobotta_rest_api/cobotta_rest_api/flask_api.py
@@ -1,21 +1,32 @@
-import rclpy
-from rclpy.node import Node
 from sensor_msgs.msg import JointState
-from flask import Blueprint
+from flask import Blueprint, request
 
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
-@bp.route("/")
+@bp.route("/move-joints")
 def moveCobotta():
+    joint_1_delta = request.args.get('joint_1', type=float)
+    joint_2_delta = request.args.get('joint_2', type=float)
+    joint_3_delta = request.args.get('joint_3', type=float)
+    joint_4_delta = request.args.get('joint_4', type=float)
+    joint_5_delta = request.args.get('joint_5', type=float)
+    joint_6_delta = request.args.get('joint_6', type=float)
+
     from .publisher_flask import node
     from .publisher_flask import publisher
     joint_state = JointState()
     joint_state.header.stamp = node.get_clock().now().to_msg()
     joint_state.name = ['joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6']
-    joint_state.position = [10.0, 2.0, 3.0, 4.0, 10.0, 3.0]
+    joint_state.position = [joint_1_delta, joint_2_delta, joint_3_delta, joint_4_delta, joint_5_delta, joint_6_delta]
     joint_state.velocity = []
     joint_state.effort = []
     publisher.publish(joint_state)
     node.get_logger().info('Publishing: "%s"' % joint_state.name)
-    return {'id' : 1}
+    return { 'joint_1': joint_1_delta,
+             'joint_2': joint_2_delta,
+             'joint_3': joint_3_delta,
+             'joint_4': joint_4_delta,
+             'joint_5': joint_5_delta,
+             'joint_6': joint_6_delta
+            }
